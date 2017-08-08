de3b10ab5b71adfb33148475ebe73d01710bf928
kuluna <mailkuluna@gmail.com>
2017-08-08 20:00:30 +0900

    check sdk path

--- src/pages/setting/setting.component.ts
+++ src/pages/setting/setting.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MdSnackBar } from '@angular/material';
 import * as settings from 'electron-settings';
+import { execFile } from 'child_process';
+import * as Path from 'path';
 
 import { AppSetting } from '../../models/models';
 
@@ -8,15 +11,32 @@ import { AppSetting } from '../../models/models';
   templateUrl: './pages/setting/setting.html'
 })
 export class SettingComponent implements OnInit {
-  public toolPath = '';
+  toolPath = '';
+  updating = false;
+
+  constructor(private snackBar: MdSnackBar) {}
 
   ngOnInit() {
     const values: any = settings.get('AppSetting', { toolPath: '' });
     this.toolPath = values.toolPath;
-    console.log(values);
   }
 
   onSubmit(value: AppSetting) {
-    settings.set('AppSetting', { toolPath: value.toolPath });
+    this.updating = true;
+
+    const normalize = Path.join(value.toolPath, 'bin', 'sdkmanager');
+    // run shell 'sdkmanager --list'
+    execFile(`${normalize}.bat`, ['--list'], (error, stdout, stderr) => {
+      // if last stdout string 'done', it is ok
+      if (/done\r?\n/.test(stdout)) {
+        settings.set('AppSetting', { toolPath: value.toolPath });
+        this.snackBar.open('Update!', undefined, { duration: 1500 });
+
+      } else {
+        this.snackBar.open('Error!');
+      }
+
+      this.updating = false;
+    });
   }
 }
